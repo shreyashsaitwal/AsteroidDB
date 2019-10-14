0c50b0985600355677b78b23406fea0fe5fa553d
Yusuf Cihan <54748183+ysfchn@users.noreply.github.com>
2019-10-14 21:24:14 +0300

    - Count() method

--- app.py
+++ app.py
@@ -130,25 +130,6 @@ def get_all():
 
 
 # -------------------------
-#  Count All Records
-#  - Returns a number that tells you how many records there are in database. 
-#  - Decrease the number with 1 if you do not want the dbpass record to be included.
-# -------------------------
-@app.route('/count')
-def count_all():
-    getpassword = TinyWebDB.query.filter_by(tag='dbpass').first()
-    if getpassword:
-        # --------------------
-        password = request.form['pass']
-        if password != getpassword.value:
-            return jsonify(['ERROR','Wrong password!'])
-        # --------------------
-    tags = TinyWebDB.query.all()
-    resl = len(tags)
-    return jsonify(['COUNT', resl])
-
-
-# -------------------------
 #  Delete Record
 #  - Delete a record from tag.
 # -------------------------
@@ -257,6 +238,18 @@ def is_true():
         # --------------------
     return jsonify(['IS CORRECT','true'])
         
+
+# -------------------------
+#  Count All Records
+#  - Returns a number that tells you how many records there are in database. 
+#  - Decrease the number with 1 if you do not want the dbpass record to be included.
+# -------------------------
+@app.route('/count')
+def count_all():
+    tags = TinyWebDB.query.all()
+    resl = len(tags)
+    return jsonify(['COUNT', resl])
+
 
 # -------------------------
 #  Is Locked?
